d5519eab48c9deda1c598a31c3a16d7614a2d164
Hao Li <haoli@cs.umd.edu>
2018-09-18 00:27:27 -0400

    mv model_file verification

--- cifar10/model_loader.py
+++ cifar10/model_loader.py
@@ -43,6 +43,7 @@ def load(model_name, model_file=None, data_parallel=False):
         net = torch.nn.DataParallel(net)
 
     if model_file:
+        assert os.path.exists(model_file), model_file + " does not exist."
         stored = torch.load(model_file, map_location=lambda storage, loc: storage)
         if 'state_dict' in stored.keys():
             net.load_state_dict(stored['state_dict'])
--- model_loader.py
+++ model_loader.py
@@ -2,7 +2,6 @@ import os
 import cifar10.model_loader
 
 def load(dataset, model_name, model_file, data_parallel=False):
-    assert os.path.exists(model_file), model_file + " does not exist."
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
     return net
