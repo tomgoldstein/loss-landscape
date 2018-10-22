72ea026ce70ec05aa29401beb5a9a8f5d6dea5ce
Hao Li <haoli@cs.umd.edu>
2018-10-22 04:54:18 -0400

    add comment

--- h5_util.py
+++ h5_util.py
@@ -5,7 +5,14 @@
 import torch
 
 def write_list(f, name, direction):
-    """Write a list of numpy arrays into the hdf5 file"""
+    """ Save the direction to the hdf5 file with name as the key
+
+        Args:
+            f: h5py file object
+            name: key name_surface_file
+            direction: a list of tensors
+    """
+
     grp = f.create_group(name)
     for i, l in enumerate(direction):
         if isinstance(l, torch.Tensor):
@@ -14,6 +21,6 @@ def write_list(f, name, direction):
 
 
 def read_list(f, name):
-    """Read a list of numpy arrays from the hdf5 file"""
+    """ Read group with name as the key from the hdf5 file and return a list numpy vectors. """
     grp = f[name]
     return [grp[str(i)] for i in range(len(grp))]
