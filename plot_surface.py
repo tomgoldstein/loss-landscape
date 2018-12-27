c5b9c974891bf77033f35718232188d10f0b59d6
tomg <tomg@cs.umd.edu>
2018-12-26 20:08:19 -0500

    removing cosine similarity calculation

--- plot_surface.py
+++ plot_surface.py
@@ -265,9 +265,9 @@ if __name__ == '__main__':
     # load directions
     d = net_plotter.load_directions(dir_file)
     # calculate the consine similarity of the two directions
-    if len(d) == 2 and rank == 0:
-        similarity = proj.cal_angle(proj.nplist_to_tensor(d[0]), proj.nplist_to_tensor(d[1]))
-        print('cosine similarity between x-axis and y-axis: %f' % similarity)
+    #if len(d) == 2 and rank == 0:
+    #    similarity = proj.cal_angle(proj.nplist_to_tensor(d[0]), proj.nplist_to_tensor(d[1]))
+    #    print('cosine similarity between x-axis and y-axis: %f' % similarity)
 
     #--------------------------------------------------------------------------
     # Setup dataloader
