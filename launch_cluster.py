3837e97616d625e40388d5ada55054080e4b1e8a
tomg <tomg@cs.umd.edu>
2019-01-13 04:51:57 -0500

    preparing for job submission

--- launch_cluster.py
+++ launch_cluster.py
@@ -16,7 +16,7 @@ opts = {   "job_name":"plt",
            "cpus_per_node":20,
            "queue":"standard",
            "delete_after_submit":True,  #  Delete the pbs shell script immediately after calling qsub?
-           "call_qsub":False
+           "call_qsub":True
         }
 
 
